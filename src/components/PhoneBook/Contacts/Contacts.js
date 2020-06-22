e6bdcd670f8375e221a0983c3fb268fb43cff0bb
Tureckiy <ev.zhdanov@gmail.com>
2020-06-22 16:25:35 +0300

    final fix

--- src/components/PhoneBook/Contacts/Contacts.js
+++ src/components/PhoneBook/Contacts/Contacts.js
@@ -2,12 +2,18 @@ import React from "react";
 import { ContactListItem } from "./ContactListItem";
 
 export const ContactList = ({ data: { clients }, onDelComponent }) => {
-  if (!clients.length) {alert('Enter first name')}
+  // if (!clients.length) {alert('Enter first name')}/
 
-  return (clients.length)&&(clients.map((client) => (
-  // return (clients.map((client) => (
-    <div className="contactList" key={client.id}>
-      <ContactListItem client={client} onDelComponent={onDelComponent}/>
+  return clients.length ? (
+    clients.map((client) => (
+      // return (clients.map((client) => (
+      <div className="contactList" key={client.id}>
+        <ContactListItem client={client} onDelComponent={onDelComponent} />
+      </div>
+    ))
+  ) : (
+    <div className="contactList">
+      <p>Contacts is not available</p>
     </div>
-  )))
+  );
 };
