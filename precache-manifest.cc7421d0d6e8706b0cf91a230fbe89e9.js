c4c0cdf24479772c0b2d87a7350092bcc9ecc795
Tureckiy <ev.zhdanov@gmail.com>
2020-06-22 16:15:09 +0300

    Updates

---
+++ precache-manifest.cc7421d0d6e8706b0cf91a230fbe89e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "898fdef6431db091baf855dbdafb4e72",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "ef563015d967c3973bd4",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.92c16cab.chunk.css"
+  },
+  {
+    "revision": "6c5332dc520e1e7d9575",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.1b04cc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.1b04cc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef563015d967c3973bd4",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.c6b1e144.chunk.js"
+  },
+  {
+    "revision": "9ce7d7921033d4a3ea76",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d2f4baf6.js"
+  }
+]);
